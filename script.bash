7fd7d9139c61bb5cbb9b39fa439e97371a13d80d
pap <pap@papik.myguest.virtualbox.org>
2024-04-21 21:51:52 +0200

    modified script

--- script.bash
+++ script.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sudo chmod -R o+w frontend
+sudo chmod 777 .
+
+current_dir=$(pwd)
+username=$(basename $(dirname "$current_dir"))
+home_dir="/home/$username"
+app_dir="$home_dir/pap_z11_szachy"
 
 sudo apt update
-sudo apt -y install postgresql
+sudo apt-get -y install postgresql
 sudo apt -y install openjdk-17-jdk
 sudo apt -y install maven
 sudo apt -y install nodejs
@@ -15,11 +20,15 @@ cd frontend
 npm install react react-dom
 cd ..
 
+cd ~postgres/
+
 sudo -u postgres psql << EOF
 CREATE DATABASE opening;
 CREATE USER konrad WITH SUPERUSER;
 ALTER USER konrad WITH PASSWORD 'konrad';
 EOF
+
+cd "$app_dir"
 
 mvn clean install
 
@@ -27,5 +36,3 @@ mvn clean install
 sudo gnome-terminal --tab --title="backend" -- bash -c "java -jar target/demo-0.0.1-SNAPSHOT.jar"
 
 sudo gnome-terminal --tab --title="frontend" -- bash -c "cd frontend && npm start" 
-
-
