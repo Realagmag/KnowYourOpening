48291062562f93377c47fc5e94aa5eb6707fb0b8
mwiecyk <milosz.wiecyk.stud@pw.edu.pl>
2024-04-21 16:09:47 +0200

    removed old files

--- src/constant.js
+++
@@ -1,6 +0,0 @@
-import { CreatePosition } from "./helper";
-
-export const initGameState = {
-  position: [await CreatePosition()],
-  turn: "w",
-};
--- src/reducer/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-const actionTypes = {
-  NEW_MOVE: "NEW_MOVE",
-};
-
-export default actionTypes;
