1dfc67e3fe79914096eea96010b9c96307dc25a4
Miłosz Więcyk <milosz.wiecyk.stud@pw.edu.pl>
2024-05-22 17:33:59 +0200

    Added popup window

--- frontend/src/Browser/Opening.js
+++ frontend/src/Browser/Opening.js
@@ -1,20 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { Unstable_Popup as BasePopup } from "@mui/base/Unstable_Popup";
+import { styled } from "@mui/system";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
+import { useState } from "react";
 
 const Opening = ({ name, id, description, deleteOpening }) => {
-  const info = () => window.alert(description);
+  const [anchor, setAnchor] = useState(null);
+
+  const handleClick = (e) => setAnchor(anchor ? null : e.currentTarget);
+
+  const handleClickAway = () => setAnchor(null);
+
+  const open = Boolean(anchor);
 
   return (
-    <div className="Opening">
-      <p>
-        {id} : {name}
-      </p>
-      <div>
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteOpening(id)} />
-        <FontAwesomeIcon icon={faCircleInfo} onClick={info} />
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="Opening">
+        <p>
+          {id} : {name}
+        </p>
+        <div>
+          <FontAwesomeIcon icon={faTrash} onClick={() => deleteOpening(id)} />
+          <FontAwesomeIcon icon={faCircleInfo} onClick={handleClick} />
+          {open && (
+            <BasePopup id={id} open={open} anchor={anchor}>
+              <PopupBody>{description}</PopupBody>
+            </BasePopup>
+          )}
+        </div>
       </div>
-    </div>
+    </ClickAwayListener>
   );
 };
 
 export default Opening;
+
+const PopupBody = styled("div")(
+  ({ theme }) => `
+  width: max-content;
+  padding: 12px 16px;
+  margin: 8px;
+  border-radius: 8px;
+  background-color: brown;
+  color: rgb(0 0 0 / 0.7);
+  box-shadow: ${
+    theme.palette.mode === "dark"
+      ? `0px 4px 8px rgb(0 0 0 / 0.7)`
+      : `0px 4px 8px rgb(0 0 0 / 0.1)`
+  };
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+  z-index: 1;
+`
+);
