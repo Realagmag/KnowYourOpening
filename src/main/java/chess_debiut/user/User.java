c8dda8492f8bc85ea2d016a3ffeda061ce57823e
Konrad <realagmag@op.pl>
2024-05-23 18:59:54 +0200

    Created basic spring security jwt authentication.

---
+++ src/main/java/chess_debiut/user/User.java
@@ -0,0 +1,59 @@
+package chess_debiut.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+@Entity
+@Table(name = "_user")
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User implements UserDetails {
+    @Id
+    @SequenceGenerator(name = "user_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String username;
+    private String password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
