852a1306602a79ddfbf853c85257316b0a4e3e09
Konrad <realagmag@op.pl>
2024-06-06 01:13:10 +0200

    Deleted unused imports.

--- src/main/java/chess_debiut/jwt/CustomUserDetails.java
+++ src/main/java/chess_debiut/jwt/CustomUserDetails.java
@@ -1,11 +1,9 @@
 package chess_debiut.jwt;
 
-import chess_debiut.user.User;
 import chess_debiut.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
